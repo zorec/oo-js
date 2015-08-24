0fc5ca3ec05cd46c9b238bb0cb3f839abfc3a11e
Gabriel Brandersky <gabo6661@gmail.com>
2015-08-24 16:14:07 +0200

    feat(ui): first version of user-interface
    - add html5-boilerplate

---
+++ js/oo-ui.js
@@ -0,0 +1,112 @@
+// "imported" from oo.js:
+// - objectModule
+// - js
+// - ruby
+var initialJsonObjects = objectModule.randomObjects();
+// var exampleObjects = [{a: 'obj1_a'}, {a: 'obj2_a', b: 'obj2_b'}, {a: 'obj3_a', c: 'obj3_c'}];
+
+// single object holding the whole state of the UI
+// including operation changing state
+var ui = {
+  // get hold of ui elements in order to avoid duplication
+  defaultObjectBox: $('#obj'),
+  objectsList: $("#objects"),
+  addObject: $('#add-object'),
+  snippet: $('#snippet'),
+  selectLanguage: $('#language'),
+  // don't prefetch, use most up to date value
+  textareasSelector: '.json-object',
+
+  jsonObjects: initialJsonObjects,
+  availableLanguages: [js, ruby],
+  // index to the previous array
+  currentLanguage: 0,
+  // drawn (ui) objects
+  objectsCount: 0,
+}
+ui.render = function() {
+  // TODO: prototype is not really used, print available properties for each object
+  objectModule.chainObjects(ui.jsonObjects);
+  for (var i = 0, l = ui.jsonObjects.length; i < l; i ++) {
+    ui.newObject(ui.jsonObjects[i], i);
+  }
+  ui.renderSnippet();
+};
+
+ui.renderSnippet = function() {
+  ui.snippet.text(
+    ui.availableLanguages[ui.currentLanguage].snippet(ui.jsonObjects)
+  );
+}
+
+ui.reorder = function() {
+  // reset
+  ui.objectsCount = 0;
+  ui.jsonObjects = [];
+  var textareas = $(ui.textareasSelector);
+  textareas.each(function(index) {
+    try {
+      ui.jsonObjects.push(JSON.parse($(this).val()));
+    }
+    catch(err) {
+      alert(err);
+    }
+  });
+  ui.renderSnippet();
+};
+
+// creates new ui object
+ui.newObject = function(jsonObject, index) {
+  jsonObject = jsonObject || {};
+  ui.objectsCount ++;
+  newObject = ui.defaultObjectBox.clone();
+  newObject.find('.label').text('Object'+ ui.objectsCount);
+  var textarea = newObject.find('.default-json-object');
+  textarea.prop('name', 'obj' + ui.objectsCount);
+  textarea.prop('class', 'json-object');
+  textarea.val(JSON.stringify(jsonObject));
+  textarea.change(function (e) {
+    try {
+      ui.jsonObjects[index] = JSON.parse(textarea.val());
+      ui.renderSnippet();
+    }
+    catch(err) {
+      alert(err);
+    }
+  });
+  ui.objectsList.append(newObject);
+};
+
+$(function() {
+  // init application
+  ui.render();
+
+  // allow to add objects
+  ui.addObject.click(function() {
+    ui.newObject();
+  });
+
+  // TODO: allow to remove object
+
+  // allow sorting of objects
+  ui.objectsList.sortable({
+    // re-render when order changed
+    update:  ui.reorder,
+    cursor: 'move',
+    connectWith: '#objects'
+    //axis: 'x',
+    // placeholder: true,
+  });
+  ui.objectsList.disableSelection();
+
+  ui.selectLanguage.change(function () {
+    ui.currentLanguage = Number(ui.selectLanguage.val());
+    ui.renderSnippet();
+  });
+});
+
+
+
+
+
+
